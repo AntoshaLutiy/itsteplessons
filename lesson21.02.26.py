c1c72a21e751bd2a92ce5f847c4e724e061ecbac
AntoshaLutiy <atv325ras@gmail.com>
2026-02-21 17:04:05 +0200

    lesson21.02.26

---
+++ lesson21.02.26.py
@@ -0,0 +1,22 @@
+"""
+def назва_функції(аргументи):
+    код фунцкції
+
+
+назва_функції(значення)
+"""
+
+def first_function():
+    print("Hello students!")
+
+first_function()
+print(first_function)
+
+
+def second_function():
+    hello = 'Hello students!'
+    name = input("What is your name? ")
+    return hello, name
+
+second_function()
+print(second_function())
